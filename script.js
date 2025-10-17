496c09ec60d3532e50454b5b678d1f609e82165e
jay <callmejaymafia@gmail.com>
2025-10-17 18:46:48 +0100

    Hamburger/mobile navmenu option added

---
+++ script.js
@@ -0,0 +1,8 @@
+ // Mobile menu toggle
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    menuBtn.classList.toggle('open');
+  });
